b217ddc3abfeb81c4cdb4b405fb0521350dda88e
Gaurav Vaidya <gaurav@ggvaidya.com>
2022-05-23 15:53:22 -0400

    Added support for counting files in parallel.

--- src/main/scala/org/renci/babel/validator/Validator.scala
+++ src/main/scala/org/renci/babel/validator/Validator.scala
@@ -5,6 +5,7 @@ import com.typesafe.scalalogging._
 import org.rogach.scallop._
 import zio._
 import org.renci.babel.validator.model.{BabelOutput, Compendium}
+import zio.blocking.Blocking
 
 import scala.collection.mutable
 import zio.console._
@@ -22,6 +23,25 @@ object Validator extends zio.App with LazyLogging {
     verify()
   }
 
+  def filterFilename(conf: Conf, filename: String): Boolean = {
+    val filteredIn = conf.filterIn.getOrElse(List())
+    val filteredOut = conf.filterOut.getOrElse(List())
+
+    if (filteredIn.nonEmpty) {
+      if (filteredIn.exists(filename.startsWith(_))) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    if (filteredOut.nonEmpty && filteredOut.exists(filename.startsWith(_))) {
+      return false;
+    }
+
+    true
+  }
+
   def retrievePairedCompendiaSummaries(babelOutput: BabelOutput, babelPrevOutput: BabelOutput): Seq[(String, Compendium#Summary, Compendium#Summary)] = {
     for {
       summary <- babelOutput.compendiaSummary
@@ -31,84 +51,33 @@ object Validator extends zio.App with LazyLogging {
     }
   }
 
-  def diffResults(conf: Conf): ZIO[Console, IOException, Unit] = {
+  def relativePercentChange(count: Long, countPrev: Long): String = {
+    val percentChange = (count - countPrev).toDouble / countPrev * 100
+    f"${count - countPrev}%+d\t$percentChange%+2.4f%%"
+  }
+
+  def diffResults(conf: Conf): ZIO[Blocking with Console, Throwable, Unit] = {
     val babelOutput = new BabelOutput(conf.babelOutput())
     val babelPrevOutput = new BabelOutput(conf.babelPrevOutput())
 
     val pairedSummaries = retrievePairedCompendiaSummaries(babelOutput, babelPrevOutput)
-    ZIO.foreach(pairedSummaries)((abc: (String, Compendium#Summary, Compendium#Summary)) => putStrLn(s" - ${abc._1}"))
-      .andThen(ZIO.succeed())
-  }
-
-  def tbd(args: List[String]) = {
-    val conf = new Conf(args)
-    val babelOutput = new BabelOutput(conf.babelOutput())
-    val babelPrevOutputOpt = new BabelOutput(conf.babelPrevOutput())
-
-    val filteredIn = conf.filterIn.getOrElse(List())
-    val filteredOut = conf.filterOut.getOrElse(List())
-
-    def filterFilename(filename: String): Boolean = {
-      if (!filteredIn.isEmpty) {
-        if (filteredIn.exists(filename.startsWith(_))) {
-          return true;
-        } else {
-          return false;
+    println("Filename\tCount\tPrevCount\tDiff\tPercentageChange")
+    ZIO.foreachPar(pairedSummaries)({
+      case (filename, summary, prevSummary) if filterFilename(conf, filename) => {
+        for {
+          count <- summary.countZIO
+          prevCount <- prevSummary.countZIO
+          _ = putStrLn(s"putStrLn: ${filename}\t${count}\t${prevCount}\t${relativePercentChange(count, prevCount)}")
+        } yield {
+          println(s"${filename}\t${count}\t${prevCount}\t${relativePercentChange(count, prevCount)}")
         }
       }
-
-      if (!filteredOut.isEmpty && !filteredOut.exists(filename.startsWith(_))) {
-        return false;
+      case (filename, _, _) if !filterFilename(conf, filename) => {
+        putStrLn(s"Skipping ${filename}")
       }
-
-      true
-    }
-
-    val runtime = Runtime.default
-    val summaries = babelOutput.compendiaSummary
-    val countsByFilename = mutable.HashMap[String, Long]()
-
-
-    //
-
-    for {
-      summary <- summaries
-      filename = summary.filename
-      to_filter = filterFilename(filename)
-    } yield {
-      if (to_filter) {
-        val count = runtime.unsafeRun(summary.countZIO)
-        countsByFilename.put(filename, count)
-        println(s"Number of lines in compendium ${summary.filename}: ${count}")
-      } else {
-        println(s"Skipping compendium ${summary.filename}")
-      }
-    }
-
-    def relativePercentChange(count: Long, countPrev: Long): String = {
-      val percentChange = (count - countPrev).toDouble / countPrev * 100
-      f"${count - countPrev}%+d\t$percentChange%+2.4f%%"
-    }
-
-    val summariesMap = summaries.groupBy(_.filename)
-    val summariesPrev = babelPrevOutputOpt.compendiaSummary
-
-    // TODO: this should actually loop over the current filename counts, so we can flag where the prev run is missing
-    // files for the new one.
-    println(s"# Comparison with ${babelPrevOutputOpt.compendiaDir}")
-    for {
-      summaryPrev <- summariesPrev
-      filename = summaryPrev.filename
-      to_filter = filterFilename(filename)
-    } yield {
-      if (to_filter) {
-        val count = countsByFilename.getOrElse[Long](filename, 0)
-        val compareToCount = runtime.unsafeRun(summaryPrev.countZIO)
-        println(s"$filename\t$count\t$compareToCount\t${relativePercentChange(count, compareToCount)}")
-      } else {
-        println(s"Skipping compendium ${filename}")
-      }
-    }
+      case abc => ZIO.fail(new RuntimeException(s"Invalid paired summary: ${abc}"))
+    })
+      .andThen(ZIO.succeed())
   }
 
   // TODO:
